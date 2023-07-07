b7b205e2790bd4c5baac089cef8ea156f520b651
analauracpaiva <analauracpaiva26@gmail.com>
2023-07-07 12:35:43 -0300

    Criação do modelo Acessorio

--- garagem/models.py
+++ garagem/models.py
@@ -12,3 +12,15 @@ class Categoria(models.Model):
 
     def __str__(self):
         return self.descricao
+    
+class Acessorio(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
+    
+    class Meta:
+        verbose_name = "Acessório"
+
+    class Meta:
+        verbose_name_plural = "Acessórios"
